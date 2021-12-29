5100bcd9c17154d5971182135bfb74e623a99ab8
Kovalev Pavel <kovalev5690@gmail.com>
2021-12-29 19:06:11 +0300

    Update plotter.go

--- cmd/plotter.go
+++ cmd/plotter.go
@@ -7,34 +7,35 @@ import (
 
 	"github.com/Pavel7004/GraphPlot/pkg/circuit"
 	"github.com/Pavel7004/GraphPlot/pkg/graph"
+	. "github.com/Pavel7004/GraphPlot/pkg/integrator/bogatskiy-Shampin"
 	. "github.com/Pavel7004/GraphPlot/pkg/integrator/euler"
-	. "github.com/Pavel7004/GraphPlot/pkg/integrator/midpoint"
+	// . "github.com/Pavel7004/GraphPlot/pkg/integrator/midpoint"
 )
 
 func main() {
 	chargeCirc := circuit.ChargeComponents{
-		SupplyVoltage:     5000,
+		SupplyVoltage:     6000,
 		Capacity:          0.001,
 		Resistance:        5000,
 		StagesCount:       5,
-		GapTriggerVoltage: 3800,
+		GapTriggerVoltage: 5700,
 	}
 	load := circuit.LoadComponents{
 		Resistance: 10000,
 	}
 	st := circuit.NewCircuit(chargeCirc, load)
 	gr := graph.NewInfoPlotter(40)
-	int := NewEulerInt(0, 60, 0.1, func(t float64, x *circuit.Circuit) {
+	int := NewShampinInt(0, 60, 0.1, func(t float64, x *circuit.Circuit) {
 		gr.AddPoint(t, x.GetLoadVoltage())
 	})
-	intMid := NewMidpointInt(0, 60, 0.1, func(t float64, x *circuit.Circuit) {
+	intMid := NewEulerInt(0, 60, 0.1, func(t float64, x *circuit.Circuit) {
 		gr.AddPoint(t, x.GetLoadVoltage())
 	})
-	int.Integrate(st)
-	gr.PrepareToAddNewPlot(color.RGBA{B: 255, A: 255})
-	st = circuit.NewCircuit(chargeCirc, load)
 	intMid.Integrate(st)
-	gr.PlotFunc(color.RGBA{R: 255, A: 255}, st.GetLoadVoltageFunc())
+	gr.PrepareToAddNewPlot(color.RGBA{G: 255, A: 255})
+	st = circuit.NewCircuit(chargeCirc, load)
+	int.Integrate(st)
+	//gr.PlotFunc(color.RGBA{R: 255, A: 255}, st.GetLoadVoltageFunc())
 	imgFile, err := os.Create("result.png")
 	if err != nil {
 		panic(err)
