0b910453f9384f3d6c96383f12d32dfae2912b09
Kovalev Pavel <kovalev5690@gmail.com>
2022-01-19 11:36:55 +0300

    Add load resistance cli argument

--- pkg/cli/parse_args.go
+++ pkg/cli/parse_args.go
@@ -8,12 +8,14 @@ var (
 	Filename string  = "result.png"
 	Step     float64 = 0.1
 	Dpi      int     = 40
+	LoadRes  int     = 9000
 )
 
 func init() {
 	getopt.FlagLong(&Filename, "output", 'o', "Output file path")
 	getopt.FlagLong(&Step, "step", 's', "Integrator step")
 	getopt.FlagLong(&Dpi, "dpi", 'd', "Plot dpi")
+	getopt.FlagLong(&LoadRes, "load-resistance", 'l', "Set load resistance")
 }
 
 func ParseArgs() {
