ddd700251521737c2ed6bdc2bf50aa9a82e3eea0
Kovalev Pavel <kovalev5690@gmail.com>
2022-01-26 19:06:30 +0300

    Add implicit euler method

---
+++ pkg/integrator/midpoint-implicit/integrator.go
@@ -0,0 +1,60 @@
+package midpointimplicit
+
+import (
+	"github.com/Pavel7004/GraphPlot/pkg/circuit"
+	"github.com/Pavel7004/GraphPlot/pkg/integrator"
+)
+
+type MidpointImpInt struct {
+	begin  float64
+	end    float64
+	step   float64
+	saveFn func(t float64, x *circuit.Circuit)
+}
+
+var _ integrator.Integrator = (*MidpointImpInt)(nil)
+
+func NewMidpointImplInt(begin, end, step float64, saveFn func(t float64, x *circuit.Circuit)) integrator.Integrator {
+	return &MidpointImpInt{
+		begin:  begin,
+		end:    end,
+		step:   step,
+		saveFn: saveFn,
+	}
+}
+
+func (si *MidpointImpInt) Integrate(circ *circuit.Circuit) {
+	var (
+		t         = si.begin
+		prevDeriv = si.GetDerivativeWithCoeff(circ.GetDerivative(), 1.0/2)
+		last      bool
+	)
+	for !last {
+		if t+si.step > si.end {
+			last = true
+			si.step = si.end - t
+		}
+		currDeriv := si.GetDerivativeWithCoeff(circ.GetDerivative(), 1.0/2)
+		k1 := si.GetSumOfDerivatives(currDeriv, prevDeriv)
+		prevDeriv = currDeriv
+		circ.ApplyDerivative(si.step, k1)
+		t += si.step
+		si.saveFn(t, circ)
+	}
+}
+
+func (si *MidpointImpInt) GetDerivativeWithCoeff(der []float64, k float64) []float64 {
+	res := make([]float64, len(der))
+	for i := range der {
+		res[i] = der[i] * k
+	}
+	return res
+}
+
+func (si *MidpointImpInt) GetSumOfDerivatives(der1, der2 []float64) []float64 {
+	res := make([]float64, len(der1))
+	for i := range res {
+		res[i] = der1[i] + der2[i]
+	}
+	return res
+}
