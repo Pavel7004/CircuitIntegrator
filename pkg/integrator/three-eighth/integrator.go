14f3b2b32520f4c5b90d70deecbe81a9171c2657
Kovalev Pavel <kovalev5690@gmail.com>
2022-01-26 19:06:56 +0300

    Add 3/8 method

---
+++ pkg/integrator/three-eighth/integrator.go
@@ -0,0 +1,53 @@
+package threeeighth
+
+import (
+	"github.com/Pavel7004/GraphPlot/pkg/circuit"
+	"github.com/Pavel7004/GraphPlot/pkg/integrator"
+)
+
+type ThreeEighthInt struct {
+	begin  float64
+	end    float64
+	step   float64
+	saveFn func(t float64, x *circuit.Circuit)
+}
+
+var _ integrator.Integrator = (*ThreeEighthInt)(nil)
+
+func NewThreeEighthInt(begin, end, step float64, saveFn func(t float64, x *circuit.Circuit)) integrator.Integrator {
+	return &ThreeEighthInt{
+		begin:  begin,
+		end:    end,
+		step:   step,
+		saveFn: saveFn,
+	}
+}
+
+func (si *ThreeEighthInt) Integrate(circ *circuit.Circuit) {
+	var (
+		t    = si.begin
+		last bool
+	)
+	for !last {
+		if t+si.step > si.end {
+			last = true
+			si.step = si.end - t
+		}
+		k1 := circ.Clone()
+		k2 := circ.Clone()
+		k3 := circ.Clone()
+		k4 := circ.Clone()
+		k2.ApplyDerivative(si.step/3, k1.GetDerivative())
+		k3.ApplyDerivative(-si.step/3, k1.GetDerivative())
+		k3.ApplyDerivative(si.step, k2.GetDerivative())
+		k4.ApplyDerivative(si.step, k1.GetDerivative())
+		k4.ApplyDerivative(-si.step, k2.GetDerivative())
+		k4.ApplyDerivative(si.step, k3.GetDerivative())
+		circ.ApplyDerivative(si.step/8, k1.GetDerivative())
+		circ.ApplyDerivative(3*si.step/8, k2.GetDerivative())
+		circ.ApplyDerivative(3*si.step/8, k3.GetDerivative())
+		circ.ApplyDerivative(si.step/8, k4.GetDerivative())
+		t += si.step
+		si.saveFn(t, circ)
+	}
+}
