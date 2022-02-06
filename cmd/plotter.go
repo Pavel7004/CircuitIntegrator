afb24e425b150c70d50f93897ef6d5c7822d1bc2
Kovalev Pavel <kovalev5690@gmail.com>
2022-02-06 17:50:07 +0300

    Remove log messages in stdout

--- cmd/plotter.go
+++ cmd/plotter.go
@@ -13,7 +13,6 @@ import (
 	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/uber/jaeger-client-go"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
-	jaegerlog "github.com/uber/jaeger-client-go/log"
 	"github.com/uber/jaeger-lib/metrics"
 
 	. "github.com/Pavel7004/GraphPlot/pkg/integrator/three-eighth"
@@ -113,10 +112,9 @@ func InitTracing() io.Closer {
 			LogSpans: true,
 		},
 	}
-	jLogger := jaegerlog.StdLogger
 	jMetricsFactory := metrics.NullFactory
 	tracer, closer, err := cfg.NewTracer(
-		jaegercfg.Logger(jLogger),
+		jaegercfg.Logger(nil),
 		jaegercfg.Metrics(jMetricsFactory),
 	)
 	if err != nil {
