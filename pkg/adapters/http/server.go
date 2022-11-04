be0211da8858e218053a5953a384984299e9f371
Kovalev Pavel <kovalev5690@gmail.com>
2022-11-04 18:24:08 +0300

    Star tserver properly

--- pkg/adapters/http/server.go
+++ pkg/adapters/http/server.go
@@ -2,14 +2,15 @@ package http
 
 import (
 	"net/http"
-	"time"
 
-	"github.com/Pavel7004/GraphPlot/pkg/infra/config"
 	"github.com/gin-gonic/gin"
+
+	"github.com/Pavel7004/GraphPlot/pkg/adapters/http/handler"
+	"github.com/Pavel7004/GraphPlot/pkg/infra/config"
 )
 
 // @title           GraphPlot
-// @version         0.1
+// @version         0.3.0
 // @description     This is a server that generates plots
 
 // @contact.name   Kovalev Pavel
@@ -22,24 +23,33 @@ type Server struct {
 	server    *http.Server
 	router    *gin.Engine
 	isRunning bool
+	isDebug   bool
+
+	handler *handler.Handler
 }
 
 func New() *Server {
-	server := new(Server)
 	cfg := config.Get()
+	server := new(Server)
 
 	server.router = gin.New()
 	server.server = &http.Server{
 		Addr:           cfg.Hostname + ":" + cfg.Port,
 		Handler:        server.router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
+		ReadTimeout:    cfg.Timeout,
+		WriteTimeout:   cfg.Timeout,
 		MaxHeaderBytes: 100 * 1024 * 8, // 100 KiB
 	}
+	server.handler = handler.New()
+	server.isDebug = true
 
 	server.prepareRouter()
 
 	return server
+}
+
+func EnableRelease() {
+	gin.SetMode(gin.ReleaseMode)
 }
 
 func (s *Server) Run() error {
@@ -47,4 +57,9 @@ func (s *Server) Run() error {
 	return s.server.ListenAndServe()
 }
 
-func (s *Server) prepareRouter() {}
+func (s *Server) prepareRouter() {
+	s.router.GET("/", s.handler.GetIndexPage)
+	s.router.GET("/gate", s.handler.OpenWebsocketConnection)
+
+	s.router.Static("/static", "./static")
+}
