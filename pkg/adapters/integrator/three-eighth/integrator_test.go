0f2cad49fe7eafdaa3a034b6cc333be126c01236
Kovalev Pavel <kovalev5690@gmail.com>
2022-11-17 21:56:48 +0300

    Add benchmark for three-eighth integrator

---
+++ pkg/adapters/integrator/three-eighth/integrator_test.go
@@ -0,0 +1,38 @@
+package threeeighth_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/Pavel7004/GraphPlot/pkg/adapters/circuit"
+	threeeighth "github.com/Pavel7004/GraphPlot/pkg/adapters/integrator/three-eighth"
+)
+
+var (
+	chargeCirc = &circuit.ChargeComponents{
+		SupplyVoltage:     6000,
+		Capacity:          0.001,
+		Resistance:        5000,
+		StagesCount:       6,
+		GapTriggerVoltage: 5700,
+		HoldingVoltage:    1,
+	}
+
+	load = &circuit.LoadComponents{
+		Resistance: 10000,
+	}
+)
+
+func BenchmarkWithoutPool(b *testing.B) {
+	int := threeeighth.NewThreeEighthInt(0, 60, 0.0001, func(t float64, x *circuit.Circuit) error {
+		return nil
+	})
+	ctx := context.Background()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		circ := circuit.New(chargeCirc, load)
+		int.Integrate(ctx, circ)
+	}
+}
